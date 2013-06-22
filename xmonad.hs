55347898be40e7739699d57aa929a45cf63c4b33
Heather <Heather@cynede.net>
2013-06-22 10:35:18 +0400

    9 desktop

--- xmonad.hs
+++ xmonad.hs
@@ -9,15 +9,17 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Util.Run(spawnPipe)
 import System.IO
 
-nWorkspaces = ["1:root","2:user","3","4:dev","5:dev","6","7","8:irc"]
+nWorkspaces = ["1:root","2:user","3","4:dev","5:dev","6","7","8:irc","9:web"]
 nManageHook = composeAll . concat $
               [[ className =? "CMake,KDevelop" --> doShift "4:dev", isFullscreen --> doFullFloat]
               ,[ className =? c --> doFloat | c <- floats ]
               ,[ className =? c --> doShift "7" | c <- trash ]
               ,[ className =? c --> doShift "8:irc" | c <- irc ]
+              ,[ className =? c --> doShift "9:web" | c <- web ]
               ] where floats = ["CMake"]
                       trash = [""]
                       irc = ["Quassel"]
+                      web = ["google-chrome"]
 
 startup :: X()
 startup = do
@@ -46,5 +48,6 @@ main = do
       ,((mod4Mask, xK_F1 ), spawn "konsole")
       ,((mod4Mask, xK_F4 ), spawn "google-chrome")
       ,((mod4Mask, xK_F5 ), spawn "kdevelop")
+      ,((mod4Mask, xK_F9 ), spawn "quasselclient")
       ,((0, xK_Print), spawn "scrot")
       ]
