ee01f9f8d318fac4654a49b90b856827ec0fcd40
Heather <Heather@live.ru>
2015-05-25 00:18:36 +0400

    better cpu & network activity display

--- xmobar.hs
+++ xmobar.hs
@@ -2,7 +2,25 @@ Config { font = "xft:Helvetica-14"
        , bgColor = "black"
        , fgColor = "grey"
        , position = TopW L 95
-       , commands = [ Run Cpu ["-L","3","-H","50","--normal","green","--high","red"] 10
+       , commands = [
+        -- network activity monitor (dynamic interface resolution)
+        Run DynNetwork     [ "--template" , "[<dev>] tx:<tx> rx:<rx>"
+                             , "--Low"      , "1000"       -- units: kB/s
+                             , "--High"     , "5000"       -- units: kB/s
+                             , "--low"      , "darkgreen"
+                             , "--normal"   , "darkorange"
+                             , "--high"     , "darkred"
+                             ] 10
+
+        -- cpu activity monitor
+        , Run MultiCpu       [ "--template" , "CPU: <total0>% : <total1>%"
+                             , "--Low"      , "50"         -- units: %
+                             , "--High"     , "85"         -- units: %
+                             , "--low"      , "darkgreen"
+                             , "--normal"   , "darkorange"
+                             , "--high"     , "darkred"
+                             ] 10
+
                     , Run Memory ["-t","Mem: <usedratio>%"] 10
                     , Run Swap [] 10
                     , Run Date "%a %b %_d %l:%M" "date" 10
@@ -25,5 +43,5 @@ Config { font = "xft:Helvetica-14"
                     ]
        , sepChar = "%"
        , alignSep = "}{"
-       , template = "%StdinReader% }{ %cpu% | %memory% %swap%  <fc=#ee9a00>%date%</fc> | %battery%"
+       , template = "%StdinReader% }{ %multicpu% %dynnetwork% | %memory% %swap%  <fc=#ee9a00>%date%</fc> | %battery%"
 }
