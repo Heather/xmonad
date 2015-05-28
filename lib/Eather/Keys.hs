d204381956a2246d3e2d4ad97059e51e53a85110
Heather <Heather@live.ru>
2015-05-28 13:26:06 +0400

    minor changes

---
+++ lib/Eather/Keys.hs
@@ -0,0 +1,34 @@
+module Eather.Keys 
+  ( myKeyBindings
+  --, (<~|)
+  ) where
+
+import XMonad
+import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Hooks.ManageDocks
+
+import XMonad.Hooks.SetWMName
+
+import XMonad.Hooks.DynamicLog
+import XMonad.Util.Run(spawnPipe)
+import System.IO
+
+--infixl 2 <~|
+
+--(<~|) :: XConfig a -> [((ButtonMask, KeySym), X ())] -> XConfig a
+--f (<~|) a = additionalKeys f a
+
+myKeyBindings :: [((ButtonMask, KeySym), X ())]
+myKeyBindings =
+  [(( mod4Mask .|. shiftMask, xK_F4), spawn "sudo shutdown -h now")--win+Shift+F4
+  ,(( mod4Mask, xK_j ), sendMessage Expand) -- win + L is logout for windows
+
+  ,((mod4Mask, xK_F1 ), spawn "gnome-terminal")
+  ,((mod4Mask, xK_F4 ), spawn "chromium")
+  ,((mod4Mask, xK_F6 ), spawn "monodevelop")
+
+  ,((mod4Mask, xK_F9 ), spawn "pidgin")
+  ,((mod4Mask, xK_F10 ),spawn "steam")
+
+  ,((0, xK_Print), spawn "scrot")
+  ]
--- xmonad.hs
+++ xmonad.hs
@@ -1,9 +1,8 @@
-{-|        Light Gnomish Xmonad config
-         Copyright (C)  2012-2014 Heather
+{-|           Light Xmonad config
+         Copyright (C)  2012-2015 Heather
 --}
 
 import XMonad
-import XMonad.Hooks.ManageHelpers (isFullscreen, doFullFloat, doCenterFloat)
 import XMonad.Util.EZConfig (additionalKeys)
 import XMonad.Hooks.ManageDocks
 
@@ -13,20 +12,9 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Util.Run(spawnPipe)
 import System.IO
 
+import Eather.Keys
+
 nWorkspaces = ["1","2","3","4","5","6","7","8","9:Trash"]
-nManageHook = composeAll . concat $
-              [[ className =? "Anjuta" --> doShift "4:dev"]
-              ,[ className =? "Monodevelop" --> doShift "5:dev"]
-              ,[ className =? "Steam" --> doShift "2:user"]
-              ,[ className =? c --> doIgnore | c <- ignore ]
-              ,[ className =? c --> doFloat | c <- floats ]
-              ,[ className =? c --> doShift "4:irc" | c <- irc ]
-              ,[ className =? c --> doShift "3:web" | c <- web ]
-              ,[ isFullscreen --> doFullFloat ]
-              ] where ignore = ["panel", "Trayer", "trayer"]
-                      floats = ["Xmessage", "Steam"]
-                      irc = ["Pidgin"]
-                      web = ["Chrome"]
 
 startup :: X()
 startup = do
@@ -43,29 +31,16 @@ layout = Full ||| tiled ||| Mirror tiled
 main = do
   xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmonad/xmobar.hs"
   xmonad $ defaultConfig {
-    modMask = mod4Mask
+    modMask = controlMask -- .|. mod4Mask
     ,terminal = "xterm"
     ,borderWidth = 1
     ,normalBorderColor = "#fbc1f3"
     ,focusedBorderColor = "#000000"
     ,workspaces = nWorkspaces
-    ,manageHook = nManageHook <+> manageHook defaultConfig
     ,layoutHook = avoidStruts  $  layout
     ,startupHook = startup
     ,logHook = dynamicLogWithPP xmobarPP {
                         	ppOutput 	= hPutStrLn xmproc
                         , 	ppTitle 	= xmobarColor "green" "" . shorten 50
                         }
-    } `additionalKeys`  
-      [(( mod4Mask .|. shiftMask, xK_F4), spawn "sudo shutdown -h now")--win+Shift+F4
-      ,(( mod4Mask, xK_j ), sendMessage Expand) -- win + L is logout for windows
-
-      ,((mod4Mask, xK_F1 ), spawn "gnome-terminal")
-      ,((mod4Mask, xK_F4 ), spawn "chromium")
-      ,((mod4Mask, xK_F6 ), spawn "monodevelop")
-
-      ,((mod4Mask, xK_F9 ), spawn "pidgin")
-      ,((mod4Mask, xK_F10 ),spawn "steam")
-
-      ,((0, xK_Print), spawn "scrot")
-      ]
+    } `additionalKeys` myKeyBindings
